1065aebc1aa50021f4ccc1e8e2ca508043194687
Andrew Braun <37201987+andrew-braun@users.noreply.github.com>
2020-07-24 14:55:05 +0400

    generating post pages

--- tutorial-part-four/gatsby-node.js
+++ tutorial-part-four/gatsby-node.js
@@ -1,8 +1,9 @@
+const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
+    const { createNodeField } = actions
     if (node.internal.type === `MarkdownRemark`) {
-        const { createNodeField } = actions
         const slug = createFilePath({ node, getNode, basePath: `pages`})
         createNodeField({
             node,
@@ -11,3 +12,30 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
         })
     }
   }
+
+exports.createPages = async ({ graphql, actions }) => {
+    const { createPage } = actions
+    const result = await graphql(`
+        query {
+            allMarkdownRemark {
+                edges {
+                    node {
+                        fields {
+                            slug
+                        }
+                    }
+                }
+            }
+        }
+    `)
+
+    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+        createPage({
+            path: node.fields.slug,
+            component: path.resolve(`./src/templates/blog-post.js`),
+            context: {
+                slug: node.fields.slug,
+            },
+        })
+    })
+}
